d5535c0026c95887fe2299352e80f9234918e451
eramoss <edulramos@outlook.com>
2023-07-08 19:03:05 -0300

    create function to open web driver

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 pub mod web_walkers;
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let results = web_walkers::amazon_walker::get_all_records("test")
+        .await
+        .unwrap();
+    dbg!(results);
 }
---
+++ src/web_walkers/mod.rs
@@ -0,0 +1,1 @@
+pub mod amazon_walker;
