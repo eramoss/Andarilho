ad7c64d2ab2fe769e1e084cc0c3ba2f73b4d4285
eramoss <edulramos@outlook.com>
2023-07-10 21:42:09 -0300

    change host of rocket

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,16 @@
 extern crate rocket;
 pub mod routes;
 pub mod web_walkers;
+use std::net::Ipv4Addr;
+
+use rocket::Config;
 use routes::*;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![amazon_routes::get_all])
+    let mut config = Config::default();
+    config.address = Ipv4Addr::new(0, 0, 0, 0).try_into().unwrap();
+    rocket::build()
+        .mount("/", routes![amazon_routes::get_all])
+        .configure(config)
 }
