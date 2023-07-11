f347384048f90d8158186131ffbdfd3826f10125
eramoss <edulramos@outlook.com>
2023-07-11 11:46:56 -0300

    add enviroment config to port of rocket

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,27 @@
 extern crate rocket;
 pub mod routes;
 pub mod web_walkers;
-use std::net::Ipv4Addr;
-
 use rocket::Config;
 use routes::*;
+use std::env;
+use std::net::Ipv4Addr;
 
 #[launch]
 fn rocket() -> _ {
     let mut config = Config::default();
+
     config.address = Ipv4Addr::new(0, 0, 0, 0).try_into().unwrap();
+    config.port = match env::var("ROCKET_PORT") {
+        Ok(port) => port
+            .parse()
+            .expect("Couldn't parse port to u16, make sure to use only numbers, Example: 8000"),
+        Err(_) => {
+            println!(
+                "Couldn't get port on environment variable, make sure you have a ROCKET_PORT var."
+            );
+            8000
+        }
+    };
     rocket::build()
         .mount("/", routes![amazon_routes::get_all])
         .configure(config)
