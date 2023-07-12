a31e2f04eb5c0b0f55ed43839db4162ae2f39df2
eramoss <edulramos@outlook.com>
2023-07-12 10:41:23 -0300

    testing multiples windows

--- src/web_walkers/tests/mod.rs
+++ src/web_walkers/tests/mod.rs
@@ -1,2 +1,3 @@
 pub mod get_urls_test;
 pub mod record_result_tests;
+pub mod start_multiple_windows;
---
+++ src/web_walkers/tests/start_multiple_windows.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod tests {
+    use crate::web_walkers::start_driver;
+
+    #[tokio::test]
+    async fn start_two_windows_same_driver() {
+        let driver = start_driver().await.unwrap();
+
+        let handle = driver.new_tab().await.unwrap();
+        driver.switch_to_window(handle).await.unwrap();
+        driver.goto("https://www.rust-lang.org").await.unwrap();
+        driver.set_window_name("rust").await.unwrap();
+
+        let handle = driver.new_tab().await.unwrap();
+        driver.switch_to_window(handle).await.unwrap();
+        driver.goto("https://github.com").await.unwrap();
+        driver.set_window_name("github").await.unwrap();
+
+        driver.switch_to_named_window("github").await.unwrap();
+        let title_github = driver.title().await.unwrap();
+
+        driver.switch_to_named_window("rust").await.unwrap();
+        let title_rust = driver.title().await.unwrap();
+
+        assert_ne!(title_rust, title_github);
+    }
+}
