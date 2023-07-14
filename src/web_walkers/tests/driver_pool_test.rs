a1f07c5b31598168fcae8ba32c2660e3c769d76a
eramoss <edulramos@outlook.com>
2023-07-14 16:03:04 -0300

    rewrite tests

--- src/web_walkers/tests/driver_pool_test.rs
+++ src/web_walkers/tests/driver_pool_test.rs
@@ -1,22 +1,34 @@
 #[cfg(test)]
 mod tests_pool {
-
     use crate::web_walkers::driver_pool::*;
+
+    const POOL_SIZE: usize = 1;
     #[tokio::test]
     async fn create_pool() {
-        let pool = WebDriverPool::new(4).await.unwrap();
-        assert_eq!(pool.workers.len(), 4);
-        for workers in pool.workers {
+        // Init pool or get it from `get_global_pool()`;
+        init_global_pool().await;
+        let pool = get_global_pool().await.unwrap();
+
+        assert_eq!(pool.workers.len(), POOL_SIZE);
+
+        for workers in &pool.workers {
             assert!(workers.status().await.unwrap().ready);
         }
     }
 
     #[tokio::test]
     async fn execute_with_worker() {
-        let mut pool = WebDriverPool::new(4).await.unwrap();
+        // Init pool or get it from `get_global_pool()`;
+        init_global_pool().await;
+        let pool = get_global_pool().await.expect("Cannot get global pool");
+
+        // Get a driver instance of pool to use
         let driver = pool.get_driver().await.unwrap();
+
         driver.goto("https://www.rust-lang.org").await.unwrap();
         assert_eq!(driver.title().await.unwrap(), "Rust Programming Language");
+
+        //after use return driver for pool
         pool.return_driver(driver);
     }
 }
